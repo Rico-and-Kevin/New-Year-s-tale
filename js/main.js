64e3f4bab69dabd2be0094d258a009020641ed20
Rico-and-Kevin <sallimaramuilam@gmail.com>
2026-01-10 20:50:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
+
+// 2. Проверка темной темы в localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+// Если меняются системные настройки, меняем тему
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+			btnDarkMode.classList.add("dark-mode-btn--active");
+			document.body.classList.add("dark");
+			localStorage.setItem("darkMode", "dark");
+		} else {
+			btnDarkMode.classList.remove("dark-mode-btn--active");
+			document.body.classList.remove("dark");
+			localStorage.setItem("darkMode", "light");
+		}
+    });
+
+// Включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'light')
+    }
+}
+
+
+function startCountdown() {
+    const targetDate = new Date('January 1, 2027 00:00:00').getTime();
+    const updateInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = targetDate - now;
+        
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById('days').innerHTML = days;
+        document.getElementById('hours').innerHTML = hours;
+        document.getElementById('minutes').innerHTML = minutes;
+        document.getElementById('seconds').innerHTML = seconds;
+    }, 1000);
+}
+
+        // Запуск таймера при загрузке страницы
+        window.onload = startCountdown;
